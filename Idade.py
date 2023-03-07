6faf28650cc3312da4d28d57b466f14ce59afddb
Fsnjoao <Fuusionsz@gmail.com>
2023-03-06 22:03:03 -0300

    Tarefas Python

---
+++ Idade.py
@@ -0,0 +1,8 @@
+idade = 18
+
+print ("João Antonio Ferreira Roth")
+print ("Rua Primeiro de Maio, 1072")
+print ("CPF: 111.345.214.21")
+if idade < 18:
+    print ("Você é Menor de idade" )
+else: print ("Você é menor de idade")
